390f63cddc3a119be5dc3e2013c8d6a77d877a58
khwilo <khwilowatai@gmail.com>
2019-04-10 21:00:10 +0300

    Add the Votes component

---
+++ src/components/Votes.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Votes = () => {
+    return (
+        <div className="votesContainer">
+            <p><i className="fa fa-caret-up" aria-hidden="true"></i></p>
+            <p>12</p>
+        </div>
+    );
+}
+
+export default Votes;
