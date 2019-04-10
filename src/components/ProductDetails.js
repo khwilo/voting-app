9394f922ffea82645c40c3116ef9a36f7d8c0d57
khwilo <khwilowatai@gmail.com>
2019-04-10 21:05:23 +0300

    Add the ProductDetails component

---
+++ src/components/ProductDetails.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ProductDetails = () => {
+    return (
+        <div className="productDetailsContainer">
+            <div className="productTitle">
+                <p>Product Title</p>
+            </div>
+            <div className="productDescription">
+                <p>Body of product description</p>
+            </div>
+        </div>
+    );
+}
+
+export default ProductDetails;
