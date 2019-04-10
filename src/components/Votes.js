d7872ded46cfb9cc02a85906d2f6670327e71085
khwilo <khwilowatai@gmail.com>
2019-04-10 21:46:35 +0300

    Add className for the upVote icon

--- src/components/Votes.js
+++ src/components/Votes.js
@@ -3,7 +3,7 @@ import React from "react";
 const Votes = () => {
     return (
         <div className="votesContainer">
-            <p><i className="fa fa-caret-up" aria-hidden="true"></i></p>
+            <p><i className="fa fa-caret-up upVote" aria-hidden="true"></i></p>
             <p>12</p>
         </div>
     );
