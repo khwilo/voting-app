f31666430471e9ae81db852f4a1f6a1a7aeda8f5
khwilo <khwilowatai@gmail.com>
2019-04-10 21:00:40 +0300

    Render the Votes component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import "../styles/App.css";
 
 import ProductImage from "./ProductImage";
+import Votes from "./Votes";
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
             </div>
             <div className="main">
                 <ProductImage />
+                <Votes />
             </div>
         </div>
     )
