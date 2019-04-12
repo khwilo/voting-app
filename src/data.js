fc4c6c13eb1769137526c43edcc7d586addf3048
khwilo <khwilowatai@gmail.com>
2019-04-12 12:13:04 +0300

    Add seed data for the products

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const products = [
+    {
+        id             : 1,
+        title          : "Cardbox",
+        description    : "Contact re-imagined as a social network",
+        avatarUrl      : "https://ui-avatars.com/api/?name=Khwilo+Kabaka",
+        productImageUrl: "https://picsum.photos/150/150/?random",
+        voteCount      : 0
+    },
+    {
+        id             : 2,
+        title          : "Emoji clipboard",
+        description    : "Click and copy the perfect emoji for any situation",
+        avatarUrl      : "https://ui-avatars.com/api/?name=John+Doe",
+        productImageUrl: "https://picsum.photos/150/150/?random",
+        voteCount      : 0
+    },
+    {
+        id             : 3,
+        title          : "Taskeo",
+        description    : "Productivity assistant for you and your team",
+        avatarUrl      : "https://ui-avatars.com/api/?name=Noel+Munyuma",
+        productImageUrl: "https://picsum.photos/150/150/?random",
+        voteCount      : 0
+    },
+    {
+        id             : 4,
+        title          : "Revere Alerts",
+        description    : "New way to monitor data and set alerts, code-free",
+        avatarUrl      : "https://ui-avatars.com/api/?name=Kyla+Imali",
+        productImageUrl: "https://picsum.photos/150/150/?random",
+        voteCount      : 0
+    }
+]
+
+export default products;
