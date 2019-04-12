ef0ab1f35e9628a1c32451a6f70be9f71b18e600
khwilo <khwilowatai@gmail.com>
2019-04-12 15:38:49 +0300

    Use object destructuring to obtain the properties

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,7 +6,7 @@ import Votes from './Votes';
 
 class Product extends Component {
     render() {
-        const { product }                                                    = this.props;
+        const { product, onUpVote }                                          = this.props;
         const { title, description, avatarUrl, productImageUrl,  voteCount } = product;
 
         return (
@@ -14,8 +14,8 @@ class Product extends Component {
                 <ProductImage productImageUrl={productImageUrl} />
                 <Votes
                     voteCount = {voteCount}
-                    onUpVote  = {this.props.onUpVote}
-                    product   = {this.props.product}
+                    onUpVote  = {onUpVote}
+                    product   = {product}
                 />
                 <ProductDetails
                     avatarUrl   = {avatarUrl}
