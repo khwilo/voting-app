c10b2e304f78d55f3d3b502cd3ff2bd6410332b8
khwilo <khwilowatai@gmail.com>
2019-04-12 11:00:46 +0300

    Render the Product component

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,11 @@ import React from "react";
 
 import "../styles/App.css";
 
-import ProductDetails from "./ProductDetails";
-import ProductImage from "./ProductImage";
-import Votes from "./Votes";
+import Product from './Product';
 
 const App = () => {
     return (
-        <div className="container">
-            <div className="header">
-                <h1>Product Listing</h1>
-            </div>
-            <div className="main">
-                <ProductImage />
-                <Votes />
-                <ProductDetails />
-            </div>
-        </div>
+        <Product />
     )
 }
 
